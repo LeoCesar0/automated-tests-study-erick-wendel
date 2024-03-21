a4d1ed2ad2629c045d43166f1a1e111f6363c7ab
Leonardo César <leo_cesa@hotmail.com>
2024-03-21 16:27:17 -0300

    feat: finish tdd class

--- modulo-04-tdd-bdd/aula-01-tdd/test/api.test.js
+++ modulo-04-tdd-bdd/aula-01-tdd/test/api.test.js
@@ -1,4 +1,4 @@
-import { it, expect, describe, beforeAll, afterAll } from "@jest/globals";
+import { it, expect, describe, beforeAll, afterAll, jest } from "@jest/globals";
 import server from "../src/api";
 import { USER_EXCEPTIONS } from "../src/static/exceptions";
 
@@ -9,21 +9,21 @@ const awaitForServerStatus = async (server) => {
   });
 };
 
-describe("Api Test Suite", () => {
+describe("Api E2E Test Suite", () => {
   let _serverAddress = "";
   let _server;
 
   beforeAll(async () => {
-    _server = server.listen()
+    _server = server.listen();
     await awaitForServerStatus(_server);
     const info = server.address();
     _serverAddress = `http://localhost:${info.port}`;
   });
 
   afterAll((done) => {
-    _server.closeAllConnections()
-    _server.close(done)
-  })
+    _server.closeAllConnections();
+    _server.close(done);
+  });
 
   const createUser = async (payload) => {
     return fetch(`${_serverAddress}/users`, {
@@ -37,49 +37,83 @@ describe("Api Test Suite", () => {
     return result.json();
   };
 
-  describe("E2E Suite", () => {
-    // --------------------------
-    // ERRORS
-    // --------------------------
+  // --------------------------
+  // ERRORS
+  // --------------------------
 
-    it("should throw an error if the user is under 18 years old", async () => {
-      const payload = {
-        name: "Fulano de Tal",
-        birthday: "2010-05-01",
-      };
-      const result = await createUser(payload);
+  it("should throw an error if the user is under 18 years old", async () => {
+    const payload = {
+      name: "Fulano de Tal",
+      birthday: "2010-05-01",
+    };
+    const result = await createUser(payload);
 
-      expect(result.status).toBe(422);
-      const data = await result.json();
+    expect(result.status).toBe(422);
+    const data = await result.json();
 
-      expect(data.errorMessage).toBe(USER_EXCEPTIONS.AGE_INVALID);
+    expect(data.errorMessage).toBe(USER_EXCEPTIONS.AGE_INVALID);
+  });
+
+  // --------------------------
+  // SUCCESS
+  // --------------------------
+
+  it("should create a user with category young-adult for users aged between 18-29", async () => {
+    const expectedCategory = "young-adult";
+    jest.useFakeTimers({
+      now: new Date("2024-03-21T00:00:00-03:00"),
     });
+    const payload = {
+      name: "Fulano de Tal",
+      birthday: "2000-05-01",
+    };
+    const result = await createUser(payload);
+    const data = await result.json();
+    expect(result.status).toBe(201);
+    expect(data.id).toBeTruthy();
 
-    // --------------------------
-    // SUCCESS
-    // --------------------------
+    const user = await getUserById(data.id);
+    expect(user.id).toBe(data.id);
+    expect(user.name).toBe(payload.name);
+    expect(user.category).toBe(expectedCategory);
+  });
 
-    it("should create a user with category young-adult for users aged between 18-29", async () => {
-      const expectedCategory = "young-adult";
-      const payload = {
-        name: "Fulano de Tal",
-        birthday: "2000-05-01",
-      };
-      const result = await createUser(payload);
-      const data = await result.json();
-      expect(result.status).toBe(201);
-      expect(data.id).toBeTruthy();
-
-      const user = await getUserById(data.id);
-      expect(user.id).toBe(data.id);
-      expect(user.name).toBe(payload.name);
-      expect(user.category).toBe(expectedCategory);
+  it("should create a user with category adult for users aged between 30-49", async () => {
+    const expectedCategory = "adult";
+    jest.useFakeTimers({
+      now: new Date("2024-03-21T00:00:00-03:00"),
     });
+    const payload = {
+      name: "Fulano de Tal",
+      birthday: "1985-05-01",
+    };
+    const result = await createUser(payload);
+    const data = await result.json();
+    expect(result.status).toBe(201);
+    expect(data.id).toBeTruthy();
 
-    it.todo(
-      "should create a user with category adult for users aged between 30-49"
-    );
-    it.todo("should create a user with category senior for users aged 50+");
-    it.todo("should throw an error if the user is under 18 years old");
+    const user = await getUserById(data.id);
+    expect(user.id).toBe(data.id);
+    expect(user.name).toBe(payload.name);
+    expect(user.category).toBe(expectedCategory);
+  });
+  it("should create a user with category senior for users aged 50+", async () => {
+    const expectedCategory = "senior";
+    jest.useFakeTimers({
+      now: new Date("2024-03-21T00:00:00-03:00"),
+    });
+    const payload = {
+      name: "Fulano de Tal",
+      birthday: "1950-05-01",
+    };
+    const result = await createUser(payload);
+    const data = await result.json();
+    expect(result.status).toBe(201);
+    expect(data.id).toBeTruthy();
+
+    const user = await getUserById(data.id);
+    expect(user.id).toBe(data.id);
+    expect(user.name).toBe(payload.name);
+    expect(user.category).toBe(expectedCategory);
   });
 });
