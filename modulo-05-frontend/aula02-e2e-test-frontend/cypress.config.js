165f5e3c703bf06a057cd884e8e65af9b8b067d9
Leonardo César <leo_cesa@hotmail.com>
2024-03-22 18:17:20 -0300

    WIP: cypress class

---
+++ modulo-05-frontend/aula02-e2e-test-frontend/cypress.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import preprocessor from "@badeball/cypress-cucumber-preprocessor";
+import createESBuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+async function setupNodeEvents(on, config) {
+  // implement node event listeners here
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createESBuildPlugin.default(config)],
+    })
+  );
+
+  return config
+}
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents: setupNodeEvents,
+    specPattern: 'cypress/e2e/features/*.feature',
+  },
+});
---
+++ modulo-05-frontend/aula02-e2e-test-frontend/cypress/e2e/common/formCommonSteps.cy.js
@@ -0,0 +1,10 @@
+import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
+
+
+Given("I am on the image registration page", () => {})
+
+When("I enter {string} in the title field", () => {})
+
+Then("I enter {string} in the URL field", () => {})
+
+Then("I click the submit button", () => {})
